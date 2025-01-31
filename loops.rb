bb766c6daea1da4001d1b3bc717f8fabd864e641
MKMaths22 <120586172+MKMaths22@users.noreply.github.com>
2025-01-31 14:42:15 +0000

    Another messy commit...Create lots of new methods/variables. Make testcode.rb to check how individual methods work.

--- loops.rb
+++ loops.rb
@@ -1,6 +1,6 @@
 class ConstructLoops
 
-  attr_reader :n, :current_parameter_limits, :current_loop_lengths, :loop_lengths
+  attr_reader :n, :current_parameter_limits, :current_loop_lengths, :loop_lengths, :current_loop_vector
   attr_accessor :current_parameter_values, :current_named_loops, :current_derangement, :current_variables, :change_from_index, :remaining_named_loop_values
 
   def initialize(n = 7)
@@ -13,9 +13,10 @@ class ConstructLoops
     @current_parameter_limits = ((n - 1).times.map { |num| (n - 1) - num }).unshift(1)
     @current_variables = []
     update_variables
-    @current_named_loops = nil
-    @current_derangement = nil
-    @change_from_index = n - 1
+    @current_loop_vector = []
+    @current_named_loops = n.times.map { |num| num + 1 }
+    @current_derangement = []
+    @change_from_index = current_variables.size - 1
     @remaining_named_loop_values = []
     # current_parameter_limits starts as [1, n - 1, n - 2 .... 1]
   end
@@ -24,6 +25,7 @@ class ConstructLoops
     write_loop_lengths
     loop_lengths[n].each do |array|
       update_current_loop_lengths(array)
+      set_parameter_limits_and_variables_etc_for_new_loop_lengths
       make_named_loops
     end
   end
@@ -48,11 +50,22 @@ class ConstructLoops
       end
   end
 
-  def make_named_loops
-    # current_loop_lengths is for example [3, 4, 2] from which we will get all derangements of n = 9 in which the element '1' is in a loop of length 3 and the next lowest
-    # element not yet chosen is in a loop of length 4 and the remaining elements make a loop of length 2.
+  def set_parameter_limits_and_variables_etc_for_new_loop_lengths
     update_parameter_limits
     update_variables
+    reset_parameter_values
+    reset_named_loops
+    update_loop_vector
+    reset_change_from_index
+    reset_remaining_named_loop_values
+    generate_initial_derangement
+  end
+  
+  def make_named_loops
+    output_derangement
+    # NOT SURE WHERE OUTPUTTING THE FIRST DERANGEMENT SHOULD GO, CHECK EXACTLY WHAT enumerate_this_and_remaining does recursively
+    # current_loop_lengths is for example [3, 4, 2] from which we will get all derangements of n = 9 in which the element '1' is in a loop of length 3 and the next lowest
+    # element not yet chosen is in a loop of length 4 and the remaining elements make a loop of length 2.
     # the parameters are numbered in the computer-indexed way from 0 to n - 1, and correspondingly the variables, which are the parameters with limit at least 2
     # the VALUES of the parameters/variables are human-indexed, where 1 means lowest-available value etc..
     update_named_loops
@@ -70,7 +83,6 @@ class ConstructLoops
 
   def update_named_loops
     enumerate_this_and_remaining(0)
-    # THEN MAKE SURE the parameter_limits, parameter_values, varaibles are RESET
   end
 
   def enumerate_this_and_remaining(variable_index)
@@ -80,7 +92,7 @@ class ConstructLoops
     parameter_number = current_variables[variable_index]
     number_of_values_taken = current_parameter_limits[parameter_number]
     number_of_values_taken.times do |cycle|
-      variable_index + 1 == current_variables.size ? modify_named_loops_and_derangement : enumerate_this_and_remaining(variable_index + 1)
+      variable_index + 1 == current_variables.size ? modify_and_output_named_loops_and_derangement : enumerate_this_and_remaining(variable_index + 1)
       if cycle + 1 == number_of_values_taken 
         current_parameter_values[parameter_number] = 1 
       else
@@ -91,14 +103,17 @@ class ConstructLoops
     end
   end
 
-  def modify_named_loops_and_derangement
-    # uses change_from_index and remaining_named_loop_values to modify what is needed. Then the modified derangement needs to go to the Outputter.
+  def modify_and_output_named_loops_and_derangement
+
+
+  
+    output_derangement
   end
 
   private
   
   def update_parameter_limits
-    # assumes we have just reset the parameter limits from the previous current_loop_lengths.
+    reset_parameter_limits
     index_to_change = 0
     current_loop_lengths.each do |length|
       index_to_change += length
@@ -117,11 +132,46 @@ class ConstructLoops
     @current_loop_lengths = array
   end
 
+  def update_loop_vector
+    # the current_loop_vector tells us how to loop back to the beginning of each loop when
+    # making a derangement from named_loops. Updating this ONCE when the loop_lengths change makes
+    # getting the next derangement easier.
+    @current_loop_vector = n.times.map { |num| num + 1 }
+    index_to_change = -1
+    current_loop_lengths.each do |length|
+      index_to_change += length
+      @current_loop_vector[index_to_change] -= length
+    end
+  end
+
   def reset_parameter_limits
     @current_parameter_limits = ((n - 1).times.map { |num| (n - 1) - num }).unshift(1)
   end
 
   def reset_parameter_values
     @current_parameter_values = Array.new(n, 1)
+  end
+
+  def reset_named_loops
+    @current_named_loops = n.times.map { |num| num + 1 }
+  end
+    
+  def reset_change_from_index
+    @change_from_index = current_variables.size - 1
+  end
+    
+  def reset_remaining_named_loop_values
+    @reset_remaining_named_loop_values = []
+  end
+
+  def generate_initial_derangement
+    @current_named_loops.each_with_index do |value, index|
+      @current_derangement[value - 1] = @current_named_loops[@current_loop_vector[index]]
+    end
+    # WE WANT TO GENERALISE THIS FOR CASES WHEN WE ARE JUST MODIFYING THE DERANGEMENT from a certain point in the named_loops onwards because of the @change_from_index 
+  end
+  
+  def output_derangement
+    outputter.add_perm_to_output(current_derangement)
   end
 end
---
+++ testcode.rb
@@ -0,0 +1,25 @@
+def loop_vector(current_loop_lengths, n)
+    # the current_loop_vector tells us how to loop back to the beginning of each loop when
+    # making a derangement from named_loops. Updating this ONCE when the loop_lengths change makes
+    # getting the next derangement easier.
+    current_loop_vector = n.times.map { |num| num + 1 }
+    index_to_change = -1
+    current_loop_lengths.each do |length|
+      index_to_change += length
+      current_loop_vector[index_to_change] -= length
+    end
+    current_loop_vector
+end
+
+def generate_initial_derangement(current_loop_lengths, n)
+    current_loop_vector = loop_vector(current_loop_lengths, n)
+    current_named_loops = n.times.map { |num| num + 1 }
+    current_derangement = Array.new(n, 0)
+    current_named_loops.each_with_index do |value, index|
+      current_derangement[value - 1] = current_named_loops[current_loop_vector[index]]
+    end
+    current_derangement
+end
+
+puts generate_initial_derangement([3,4,2], 9)
+
